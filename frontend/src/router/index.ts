ae9f8ecb73d3c75c8096453a02ba92469274ab8f
Icaro Oliveira <icaro.oliveira@clinicarx.com.br>
2022-09-08 19:16:51 -0300

    Voltando com as view de error-page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,9 +6,6 @@ import Layout from '@/layout/index.vue'
 
 /* Router modules */
 import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
 
 Vue.use(VueRouter)
 
