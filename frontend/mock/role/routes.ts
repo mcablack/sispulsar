71c682ab96327cfa49162e2f385e5b3b6df0e539
Icaro Oliveira <icaro.oliveira@clinicarx.com.br>
2022-09-08 02:27:57 -0300

    Limpando rota i18n

--- frontend/mock/role/routes.ts
+++ frontend/mock/role/routes.ts
@@ -368,21 +368,6 @@ export const asyncRoutes = [
       }
     ]
   },
-  // {
-  //   path: '/i18n',
-  //   component: 'Layout',
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       component: 'views/i18n-demo/index',
-  //       name: 'I18n',
-  //       meta: {
-  //         title: 'i18n',
-  //         icon: 'international'
-  //       }
-  //     }
-  //   ]
-  // },
   {
     path: '*',
     redirect: '/404',
