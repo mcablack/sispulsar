a8bd04081db718b4f32639dbaf5e9b00adf9d9ed
Icaro Oliveira <icaro.oliveira@clinicarx.com.br>
2022-09-08 02:09:52 -0300

    Removendo view theme e suas rotas

--- frontend/mock/role/routes.ts
+++ frontend/mock/role/routes.ts
@@ -516,22 +516,6 @@ export const asyncRoutes = [
     meta: { hidden: true }
   },
   {
-    path: '/theme',
-    component: 'Layout',
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'index',
-        component: 'views/theme/index',
-        name: 'Theme',
-        meta: {
-          title: 'theme',
-          icon: 'theme'
-        }
-      }
-    ]
-  },
-  {
     path: '/i18n',
     component: 'Layout',
     children: [
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -346,22 +346,6 @@ export const asyncRoutes: RouteConfig[] = [
     meta: { hidden: true }
   },
   {
-    path: '/theme',
-    component: Layout,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "theme" */ '@/views/theme/index.vue'),
-        name: 'Theme',
-        meta: {
-          title: 'theme',
-          icon: 'theme'
-        }
-      }
-    ]
-  },
-  {
     path: '/i18n',
     component: Layout,
     children: [
